90ac5d15e136af4034aa2627e26ff1129368e2e0
RFSurdsman <ryan_fallah@hotmail.com>
2018-09-22 14:43:42 +1000

    ksndlksajdas

--- user.py
+++ user.py
@@ -1,12 +1,12 @@
 from flask_login import UserMixin
 
 class User(UserMixin):
-    def __init__(self, username, password, path):
+    def __init__(self, username, password, path, position):
         self.id = id(self)
         self.username = username
         self.password = password
         self.friends = []
-        self.position = (0, 0)
+        self.position = position
         self.imagelocation = path
 
     def update_position(self, longitude, latitude):
@@ -23,4 +23,11 @@ class User(UserMixin):
 
     def get_ID(self):
         return self.id
+
+    def get_map_pixels(self):
+        xDiff = self.position[0] - 33.918216
+        yDiff = self.position[1] - 151.231402
+
+        return (xDiff * 250,000, yDiff * 4000)
+
 
