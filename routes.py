44b4a4b7d170144b56568c86558638a5da769b8e
RFSurdsman <ryan_fallah@hotmail.com>
2018-09-22 10:49:31 +1000

    login

--- routes.py
+++ routes.py
@@ -1,4 +1,8 @@
-from flask import render_template
+from flask import Flask, render_template, request, jsonify, _app_ctx_stack
+
+app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route('/')
-	
+def login():
+    return render_template("index.html")
