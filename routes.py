90a0531bb29b86e4f3c9e0ba4a002bcc1329274a
RFSurdsman <ryan_fallah@hotmail.com>
2018-09-22 11:18:32 +1000

    kasjlkasjD'SDA

--- routes.py
+++ routes.py
@@ -1,22 +1,23 @@
-from flask import render_template, request, url_for
-from flask_login import logout_user, current_user
+from flask import render_template, request, url_for, redirect
+from flask_login import logout_user, current_user, login_required, login_user
 from user import *
+from server import *
 
 users = []
 
 def validate_login(username, password):
 	for u in users:
-		if username == u.username and password == p.password
-		return user
+		if username == u.username and password == u.password:
+		    return u
 	return None	
 
 @app.route('/')
 def index():
-	return render_template()
+	return render_template("index.html")
 
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
+def loginPage():
 	if request.method == 'POST':
 		username = request.form['Username']
 		password = request.form['Password']
@@ -27,13 +28,13 @@ def login():
 		if user is None:
 			return
 		else:
-			login_user
-			redirect(url_for('map'))
+			login_user(user)
+			return redirect(url_for('map'))
 
 @app.route('/map')
 @login_required
 def map():
-
+    pass
 
 
 
@@ -43,7 +44,8 @@ def logout():
 	logout_user()
 	return redirect(url_for('index'))
 
-@app.route('/register', methods=['GET'. 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
+def register():
 	if request.method == 'POST':
 		username = request.form['Username']
 		password = request.form['Password']
